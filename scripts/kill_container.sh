603cdb313333bf0141eee7a050e730fb4d87b956
dedadhic <dadhich.dev@gmail.com>
2019-05-18 17:36:11 +0530

    intial checkin

---
+++ scripts/kill_container.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Removing exiting docker container"
+sudo docker stop demo-codepipeline-ecr && docker rm -f demo-codepipeline-ecr || true
+echo "Removing exiting docker image"
+sudo docker images | grep "demo-codepipeline-ecr" | awk '{print $1":"$2}' | xargs docker rmi
---
+++ scripts/start_container.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+echo "Logging in to Amazon ECR..."
+$(aws ecr get-login --no-include-email --region us-west-2)
+echo "Running docker image"
+sudo docker run -d --name cdr-dashboard -p 8443:8443 628443245197.dkr.ecr.us-west-2.amazonaws.com/demo-codepipeline-ecr:latest
